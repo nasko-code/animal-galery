586176e4f2b024a9851a3fb0955fee4a8f67a4d0
nasko-code <60343223+nasko-code@users.noreply.github.com>
2020-01-28 11:09:47 +0200

    add snimki

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
     <h1>Animal Gallery</h1>
 
     <div class="galery">
+    <img src="images/_108557884_gettyimages-486237421.jpg" alt="tigar">
+    <img src="images/cargo_live_animals_parrot.jpg" alt="tigar">
+    <img src="images/south-china-tiger.adapt.1900.1.jpg" alt="tigar">
     
     </div>
 </body>
